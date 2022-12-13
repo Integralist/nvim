623dfd51661f674b7bf5761bd93cc176b44dbfea
Integralist <mark.mcdx@gmail.com>
2022-12-13 19:52:39 +0000

    style(ui): automated formatting

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,7 @@
 return function(use)
   -- status line
-  use { "nvim-lualine/lualine.nvim",
+  use {
+    "nvim-lualine/lualine.nvim",
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
     config = function()
       require("lualine").setup({
@@ -11,18 +12,17 @@ return function(use)
               file_status = true, -- displays file status (readonly status, modified status)
               path = 1, -- relative path
               shorting_target = 40 -- Shortens path to leave 40 space in the window
-            },
-          },
+            }
+          }
         }
       })
     end
   }
 
   -- ui improvements
-  use { "stevearc/dressing.nvim",
-    config = function()
-      require("dressing").setup()
-    end
+  use {
+    "stevearc/dressing.nvim",
+    config = function() require("dressing").setup() end
   }
 
   -- NOTE: `:Noice` to open message history + `:Noice telescope` to open message history in Telescope.
@@ -33,37 +33,28 @@ return function(use)
       require("noice").setup({
         views = {
           cmdline_popup = {
-            size = {
-              width = "40%",
-              height = "auto",
-            },
+            size = { width = "40%", height = "auto" },
             win_options = {
               winhighlight = {
                 Normal = "Normal",
                 FloatBorder = "DiagnosticSignInfo",
                 IncSearch = "",
-                Search = "",
-              },
-            },
+                Search = ""
+              }
+            }
           },
           popupmenu = {
             relative = "editor",
-            position = {
-              row = 8,
-              col = "50%",
-            },
-            size = {
-              width = 100,
-              height = 10,
-            },
-            border = {
-              style = "rounded",
-              padding = { 0, 0.5 },
-            },
+            position = { row = 8, col = "50%" },
+            size = { width = 100, height = 10 },
+            border = { style = "rounded", padding = { 0, 0.5 } },
             win_options = {
-              winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticSignInfo" },
-            },
-          },
+              winhighlight = {
+                Normal = "Normal",
+                FloatBorder = "DiagnosticSignInfo"
+              }
+            }
+          }
         },
         routes = {
           -- skip displaying message that file was written to.
@@ -71,54 +62,43 @@ return function(use)
             filter = {
               event = "msg_show",
               kind = "",
-              find = "written",
+              find = "written"
             },
-            opts = { skip = true },
-          },
+            opts = { skip = true }
+          }
         },
-        presets = {
-          long_message_to_split = true,
-          lsp_doc_border = true,
-        },
+        presets = { long_message_to_split = true, lsp_doc_border = true },
         documentation = {
           opts = {
             win_options = {
-              winhighlight = {
-                FloatBorder = "DiagnosticSignInfo",
-              },
-            },
-          },
+              winhighlight = { FloatBorder = "DiagnosticSignInfo" }
+            }
+          }
         },
         lsp = {
           progress = {
-            enabled = false, -- I already use fidget configured in ./lsp.lua
+            enabled = false -- I already use fidget configured in ./lsp.lua
           }
         }
       })
     end,
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    }
+    requires = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" }
   })
 
   -- tab ui improvements
-  use { "akinsho/bufferline.nvim",
+  use {
+    "akinsho/bufferline.nvim",
     tag = "v2.*",
     requires = "kyazdani42/nvim-web-devicons",
     config = function()
       require("bufferline").setup({
-        options = {
-          mode = "tabs"
-        },
+        options = { mode = "tabs" },
         highlights = {
-          tab = {
-            fg = "#CCCCCC",
-          },
+          tab = { fg = "#CCCCCC" }
           -- tab_selected = {
           --   fg = "#FF0000"
           -- },
-        },
+        }
       })
     end
   }
@@ -132,11 +112,7 @@ return function(use)
   --
   -- <Ctrl-f> scroll down
   -- <Ctrl-b> scroll up
-  use { "junegunn/fzf",
-    run = function()
-      vim.fn["fzf#install"]()
-    end
-  }
+  use { "junegunn/fzf", run = function() vim.fn["fzf#install"]() end }
   use { "kevinhwang91/nvim-bqf", ft = "qf" }
 
   -- window bar breadcrumbs
@@ -144,17 +120,13 @@ return function(use)
     "SmiteshP/nvim-navic",
     requires = "neovim/nvim-lspconfig",
     config = function()
-      require("nvim-navic").setup({
-        highlight = true,
-      })
+      require("nvim-navic").setup({ highlight = true })
     end
   }
 
   -- scrollbar
   use {
     "petertriho/nvim-scrollbar",
-    config = function()
-      require("scrollbar").setup()
-    end
+    config = function() require("scrollbar").setup() end
   }
 end
