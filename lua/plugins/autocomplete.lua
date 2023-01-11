2150ee7ba7599bd9f3968fda5afd2381f1171c9a
Integralist <mark.mcdx@gmail.com>
2023-01-11 10:08:51 +0000

    feature(autocomplete): add neovim dev plugin for better lua completion

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -5,17 +5,15 @@ return function(use)
       local cmp = require("cmp")
 
       cmp.setup({
-        experimental = {
-          ghost_text = true,
-        },
+        experimental = { ghost_text = true },
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
-          end,
+          end
         },
         window = {
           completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered()
         },
         mapping = {
           ["<Up>"] = cmp.mapping.select_prev_item(),
@@ -24,37 +22,30 @@ return function(use)
           ["<Right>"] = cmp.mapping.select_next_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(),
+            { "i", "c" }),
           ["<C-e>"] = cmp.mapping.close(),
           ["<CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
+            select = true
           })
         },
         sources = cmp.config.sources({
           -- ordered by priority
-          { name = "nvim_lsp" },
-          { name = "nvim_lsp_signature_help" },
-          { name = "path" },
-          { name = "buffer" },
-          { name = "luasnip" },
-          { name = "nvim_lua" },
-        }),
+          { name = "nvim_lsp" }, { name = "nvim_lsp_signature_help" },
+          { name = "path" }, { name = "buffer" }, { name = "luasnip" },
+          { name = "nvim_lua" }
+        })
       })
 
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(), -- Tab for selection (arrows needed for selecting past items)
-        sources = {
-          { name = "buffer" }
-        }
+        sources = { { name = "buffer" } }
       })
 
       cmp.setup.cmdline({ ":" }, {
         mapping = cmp.mapping.preset.cmdline(), -- Tab for selection (arrows needed for selecting past items)
-        sources = {
-          { name = "cmdline" },
-          { name = "path" }
-        }
+        sources = { { name = "cmdline" }, { name = "path" } }
       })
     end
   }
@@ -70,9 +61,12 @@ return function(use)
     config = function()
       local keymap = vim.api.nvim_set_keymap
       local opts = { noremap = true, silent = true }
-      keymap("i", "<leader><leader>'", "<cmd>lua require('luasnip').jump(1)<CR>", opts)
-      keymap("i", "<leader><leader>;", "<cmd>lua require('luasnip').jump(-1)<CR>", opts)
+      keymap("i", "<leader><leader>'",
+        "<cmd>lua require('luasnip').jump(1)<CR>", opts)
+      keymap("i", "<leader><leader>;",
+        "<cmd>lua require('luasnip').jump(-1)<CR>", opts)
       require("luasnip.loaders.from_lua").load({ paths = "~/.snippets" })
     end
   }
+  use { "folke/neodev.nvim", config = function() require("neodev").setup() end }
 end
