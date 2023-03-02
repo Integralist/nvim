d69c6601c57680500166c035cbd46d8fe8f5e7a9
Integralist <mark.mcdx@gmail.com>
2023-03-02 13:40:50 +0000

    switch from packer to folke/lazy.nvim

--- lua/plugins/repeat.lua
+++
@@ -1,4 +0,0 @@
-return function(use)
-  -- make dot operator work in a sensible way
-  use "tpope/vim-repeat"
-end
--- lua/settings/highlights.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  group = vim.api.nvim_create_augroup("ScrollbarHandleHighlight", { clear = true }),
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_set_hl(0, "ScrollbarHandle", { fg = "#ff0000", bg = "#8ec07c" })
-  end,
-})
