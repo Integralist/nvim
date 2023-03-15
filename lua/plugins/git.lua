4404112cb90c49611eea45c6fb5b76ace99ed793
Integralist <mark.mcdx@gmail.com>
2023-03-15 14:29:00 +0000

    vim-gh-line keymap config variable didn't work

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,68 +1,77 @@
 return {
-  {
-    -- GIT CHANGE INDICATOR
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      vim.api.nvim_set_hl(0, "GitSignsChange",
-        { link = "GruvboxYellowSign" })
+    {
+        -- GIT CHANGE INDICATOR
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            vim.api.nvim_set_hl(0, "GitSignsChange",
+                                {link = "GruvboxYellowSign"})
 
-      require("gitsigns").setup({
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
+            require("gitsigns").setup({
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
 
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
 
-          map('n', ']c', function()
-            if vim.wo.diff then return ']c' end
-            vim.schedule(function()
-              gs.next_hunk()
-            end)
-            return '<Ignore>'
-          end, { desc = "next change hunk", expr = true })
+                    map('n', ']c', function()
+                        if vim.wo.diff then return ']c' end
+                        vim.schedule(function()
+                            gs.next_hunk()
+                        end)
+                        return '<Ignore>'
+                    end, {desc = "next change hunk", expr = true})
 
-          map('n', '[c', function()
-            if vim.wo.diff then return '[c' end
-            vim.schedule(function()
-              gs.prev_hunk()
-            end)
-            return '<Ignore>'
-          end, { desc = "prev change hunk", expr = true })
+                    map('n', '[c', function()
+                        if vim.wo.diff then return '[c' end
+                        vim.schedule(function()
+                            gs.prev_hunk()
+                        end)
+                        return '<Ignore>'
+                    end, {desc = "prev change hunk", expr = true})
 
-          map('n', '<leader><leader>gb',
-            function()
-              gs.blame_line { full = true }
-            end, { desc = "git blame" })
+                    map('n', '<leader><leader>gb',
+                        function()
+                        gs.blame_line {full = true}
+                    end, {desc = "git blame"})
 
-          map('n', '<leader><leader>gs',
-            function() gs.blame_line {} end,
-            { desc = "git blame short" })
+                    map('n', '<leader><leader>gs',
+                        function() gs.blame_line {} end,
+                        {desc = "git blame short"})
 
-          map('n', '<leader><leader>gd', gs.diffthis,
-            { desc = "git diff (:q to close)" })
+                    map('n', '<leader><leader>gd', gs.diffthis,
+                        {desc = "git diff (:q to close)"})
+                end
+            })
         end
-      })
-    end
-  }, {
-    -- GIT HISTORY
-    "sindrets/diffview.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("diffview").setup()
-      vim.keymap.set("n", "<leader><leader>gh",
-        "<Cmd>DiffviewFileHistory<CR>",
-        { desc = "diff history" })
-      vim.keymap.set("n", "<leader><leader>go", "<Cmd>DiffviewOpen<CR>",
-        { desc = "diff open" })
-      vim.keymap.set("n", "<leader><leader>gc", "<Cmd>DiffviewClose<CR>",
-        { desc = "diff close" })
-    end
-  }, {
-    -- OPEN LINES IN GITHUB
-    "ruanyl/vim-gh-line",
-    config = function() vim.g.gh_line_map = "<leader><leader>gl" end
-  }
+    }, {
+        -- GIT HISTORY
+        "sindrets/diffview.nvim",
+        dependencies = {"nvim-lua/plenary.nvim"},
+        config = function()
+            require("diffview").setup()
+            vim.keymap.set("n", "<leader><leader>gh",
+                           "<Cmd>DiffviewFileHistory<CR>",
+                           {desc = "diff history"})
+            vim.keymap.set("n", "<leader><leader>go", "<Cmd>DiffviewOpen<CR>",
+                           {desc = "diff open"})
+            vim.keymap.set("n", "<leader><leader>gc", "<Cmd>DiffviewClose<CR>",
+                           {desc = "diff close"})
+        end
+    }, {
+        -- OPEN LINES IN GITHUB
+        "ruanyl/vim-gh-line",
+        keys = {
+            {
+                "<leader><leader>gl",
+                function() vim.cmd("GH") end,
+                desc = "Open single line in GitHub's web UI",
+                noremap = true,
+                silent = true
+            }
+        },
+        config = true
+    }
 }
