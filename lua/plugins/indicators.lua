00dcded2f1f16210da782bdf34944514e249581b
Integralist <mark.mcdx@gmail.com>
2023-03-27 12:50:10 +0100

    fix(indicators): eyeliner wasn't running

--- lua/plugins/indicators.lua
+++ lua/plugins/indicators.lua
@@ -3,15 +3,14 @@ return {
     -- WORD USAGE HIGHLIGHTER
     "RRethy/vim-illuminate"
   }, {
-    -- JUMP TO WORD INDICTORS
-    "jinh0/eyeliner.nvim",
-    opts = { highlight_on_key = true, dim = true },
-    config = true
-  }, {
-    -- CURSOR MOVEMENT HIGHLIGHTER
-    "DanilaMihailov/beacon.nvim"
-  }, {
-    -- HIGHLIGHT YANKED REGION
-    "machakann/vim-highlightedyank"
-  }
+  -- JUMP TO WORD INDICTORS
+  "jinh0/eyeliner.nvim",
+  opts = { highlight_on_key = false, dim = true }
+}, {
+  -- CURSOR MOVEMENT HIGHLIGHTER
+  "DanilaMihailov/beacon.nvim"
+}, {
+  -- HIGHLIGHT YANKED REGION
+  "machakann/vim-highlightedyank"
+}
 }
