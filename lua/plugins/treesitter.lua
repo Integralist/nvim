18c1f201877f7adfc7fe7f9fb51916eb5f52aeab
Integralist <mark.mcdx@gmail.com>
2023-06-05 10:29:21 +0100

    remove spellsitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -42,77 +42,73 @@ return {
       })
     end
   }, {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        incremental_selection = {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",                  -- start treesitter selection process
+          scope_incremental = "gnm",               -- increment selection to surrounding scope
+          node_incremental = ";",                  -- increment selection to next 'node'
+          node_decremental = ","                   -- decrement selection to prev 'node'
+        }
+      },
+      indent = { enable = true },
+      textobjects = {
+        select = {
           enable = true,
+          lookahead = true,
+          include_surrounding_whitespace = false,
           keymaps = {
-            init_selection = "gnn", -- start treesitter selection process
-            scope_incremental = "gnm", -- increment selection to surrounding scope
-            node_incremental = ";", -- increment selection to next 'node'
-            node_decremental = "," -- decrement selection to prev 'node'
-          }
-        },
-        indent = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            include_surrounding_whitespace = false,
-            keymaps = {
-              ["af"] = {
-                query = "@function.outer",
-                desc = "select around a function"
-              },
-              ["if"] = {
-                query = "@function.inner",
-                desc = "select inner part of a function"
-              },
-              ["ac"] = {
-                query = "@class.outer",
-                desc = "select around a class"
-              },
-              ["ic"] = {
-                query = "@class.inner",
-                desc = "select inner part of a class"
-              }
+            ["af"] = {
+              query = "@function.outer",
+              desc = "select around a function"
             },
-            selection_modes = {
-              ['@parameter.outer'] = 'v',
-              ['@function.outer'] = 'V',
-              ['@class.outer'] = '<c-v>'
+            ["if"] = {
+              query = "@function.inner",
+              desc = "select inner part of a function"
+            },
+            ["ac"] = {
+              query = "@class.outer",
+              desc = "select around a class"
+            },
+            ["ic"] = {
+              query = "@class.inner",
+              desc = "select inner part of a class"
             }
           },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]]"] = "@function.outer",
-              ["]\\"] = "@class.outer"
-            },
-            goto_previous_start = {
-              ["[["] = "@function.outer",
-              ["[\\"] = "@class.outer"
-            }
+          selection_modes = {
+            ['@parameter.outer'] = 'v',
+            ['@function.outer'] = 'V',
+            ['@class.outer'] = '<c-v>'
+          }
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]]"] = "@function.outer",
+            ["]\\"] = "@class.outer"
+          },
+          goto_previous_start = {
+            ["[["] = "@function.outer",
+            ["[\\"] = "@class.outer"
           }
         }
-      })
-    end
-  }, {
-    -- TREESITTER POWERED SPELLCHECKER
-    "lewis6991/spellsitter.nvim",
-    config = true
-  }, {
-    -- HIGHLIGHT ARGUMENTS' DEFINITIONS AND USAGES, USING TREESITTER
-    "m-demare/hlargs.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = true
-  }, {
-    -- SHOWS THE CONTEXT OF THE CURRENTLY VISIBLE BUFFER CONTENTS
-    "nvim-treesitter/nvim-treesitter-context",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    opts = { separator = "-" }
-  }
+      }
+    })
+  end
+}, {
+  -- HIGHLIGHT ARGUMENTS' DEFINITIONS AND USAGES, USING TREESITTER
+  "m-demare/hlargs.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = true
+}, {
+  -- SHOWS THE CONTEXT OF THE CURRENTLY VISIBLE BUFFER CONTENTS
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  opts = { separator = "-" }
+}
 }
