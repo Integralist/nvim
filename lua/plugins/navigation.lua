2dd24bfea292b337f61a2975fb03c48c745e3af8
Integralist <mark.mcdx@gmail.com>
2023-07-17 19:19:32 +0100

    bump to v3 of neo-tree

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -2,6 +2,7 @@ return {
     {
         -- NAVIGATION
         "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
         dependencies = {
             "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim"
