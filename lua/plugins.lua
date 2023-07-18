867c9947a7ba007b9d878fa6d2beb28633f0ebc2
Integralist <mark.mcdx@gmail.com>
2023-07-18 19:56:58 +0100

    bump deps

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,8 @@
 return {
   "folke/neodev.nvim", { "folke/which-key.nvim", config = true }, {
-    "EdenEast/nightfox.nvim",
-    lazy = false, -- make sure we load this during startup as it is our main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function() vim.cmd([[colorscheme nightfox]]) end
-  }
+  "EdenEast/nightfox.nvim",
+  lazy = false,          -- make sure we load this during startup as it is our main colorscheme
+  priority = 1000,       -- make sure to load this before all the other start plugins
+  config = function() vim.cmd("colorscheme nightfox") end
+}
 }
