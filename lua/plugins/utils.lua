98ce512e9891c77046d9dbf3eaff5ab881d8caf9
Integralist <mark.mcdx@gmail.com>
2023-11-08 13:49:05 +0000

    async dispatch

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -3,34 +3,37 @@ return {
     -- MAKE DOT OPERATOR WORK IN A SENSIBLE WAY
     "tpope/vim-repeat"
   }, {
-    -- SWAPPABLE ARGUMENTS AND LIST ELEMENTS
-    "mizlan/iswap.nvim",
-    config = true
-  }, {
-    -- BLOCK SORTER
-    "chiedo/vim-sort-blocks-by"
-  }, {
-    -- MODIFY SURROUNDING CHARACTERS
-    "kylechui/nvim-surround",
-    config = true
-  }, {
-    -- CODE COMMENTS
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
+  -- SWAPPABLE ARGUMENTS AND LIST ELEMENTS
+  "mizlan/iswap.nvim",
+  config = true
+}, {
+  -- BLOCK SORTER
+  "chiedo/vim-sort-blocks-by"
+}, {
+  -- MODIFY SURROUNDING CHARACTERS
+  "kylechui/nvim-surround",
+  config = true
+}, {
+  -- CODE COMMENTS
+  "numToStr/Comment.nvim",
+  config = function()
+    require("Comment").setup()
 
-      vim.keymap.set("n", "<leader><leader><leader>", "<Cmd>norm gcc<CR>",
-        { desc = "comment a single line" })
-      vim.keymap.set("v", "<leader><leader><leader>",
-        "<Plug>(comment_toggle_linewise_visual)",
-        { desc = "comment multiple lines" })
-    end
-  }, {
-    -- DISPLAY HEX COLOURS
-    "norcalli/nvim-colorizer.lua",
-    config = function() require("colorizer").setup() end
-  }, {
-    -- GENERATE HEX COLOURS
-    "uga-rosa/ccc.nvim"
-  }
+    vim.keymap.set("n", "<leader><leader><leader>", "<Cmd>norm gcc<CR>",
+      { desc = "comment a single line" })
+    vim.keymap.set("v", "<leader><leader><leader>",
+      "<Plug>(comment_toggle_linewise_visual)",
+      { desc = "comment multiple lines" })
+  end
+}, {
+  -- DISPLAY HEX COLOURS
+  "norcalli/nvim-colorizer.lua",
+  config = function() require("colorizer").setup() end
+}, {
+  -- GENERATE HEX COLOURS
+  "uga-rosa/ccc.nvim"
+}, {
+  -- ASYNC DISPATCH
+  "tpope/vim-dispatch"
+}
 }
