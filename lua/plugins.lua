cb8dfcff5ff500690b9956e6e0c6a2c635d75ecc
Integralist <mark.mcdx@gmail.com>
2023-11-13 09:34:37 +0000

    use vi to edit plugins.lua to fix formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,28 +3,28 @@ return {
     -- NEOVIM DEVELOPMENT SETUP
     "folke/neodev.nvim"
   }, {
-  -- MAPPING IDENTIFIER
-  "folke/which-key.nvim",
-  config = function()
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>d"] = { name = "TODO" },
-      ["<leader>l"] = { name = "lsp" },
-      ["<leader><leader>d"] = { name = "dap" },
-      ["<leader><leader>g"] = { name = "git" },
-      ["<leader><leader>l"] = { name = "lsp" },
-      -- ["<leader><leader>n"] = {name = "noice"},
-      ["<leader><leader>o"] = { name = "outline" },
-      ["<leader><leader>p"] = { name = "plugins" },
-      ["<leader><leader>q"] = { name = "quickfix" },
-      ["<leader><leader>t"] = { name = "terminal" }
-    })
-  end
-}, {
-  -- COLORSCHEME
-  "EdenEast/nightfox.nvim",
-  lazy = false,          -- make sure we load this during startup as it is our main colorscheme
-  priority = 1000,       -- make sure to load this before all the other start plugins
-  config = function() vim.cmd("colorscheme nightfox") end
-}
+    -- MAPPING IDENTIFIER
+    "folke/which-key.nvim",
+    config = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>d"] = { name = "TODO" },
+        ["<leader>l"] = { name = "lsp" },
+        ["<leader><leader>d"] = { name = "dap" },
+        ["<leader><leader>g"] = { name = "git" },
+        ["<leader><leader>l"] = { name = "lsp" },
+        -- ["<leader><leader>n"] = {name = "noice"},
+        ["<leader><leader>o"] = { name = "outline" },
+        ["<leader><leader>p"] = { name = "plugins" },
+        ["<leader><leader>q"] = { name = "quickfix" },
+        ["<leader><leader>t"] = { name = "terminal" }
+      })
+    end
+  }, {
+    -- COLORSCHEME
+    "EdenEast/nightfox.nvim",
+    lazy = false,          -- make sure we load this during startup as it is our main colorscheme
+    priority = 1000,       -- make sure to load this before all the other start plugins
+    config = function() vim.cmd("colorscheme nightfox") end
+  }
 }
