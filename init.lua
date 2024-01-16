0356b22159067414551673c6b852c468d07d251d
Integralist <mark.mcdx@gmail.com>
2024-01-16 14:28:24 +0000

    refactor(init): make lazy initialisation less magic

--- init.lua
+++ init.lua
@@ -1,15 +1,23 @@
 -- ~/.local/share/nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-        lazypath
-    })
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+    lazypath
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  checker = {
+    enabled = true, -- automatically check for plugin updates
+    notify = true
+  },
+  spec = {
+    { import = "plugins" }
+  },
+})
 require("autocommands")
 require("mappings")
 require("settings")
