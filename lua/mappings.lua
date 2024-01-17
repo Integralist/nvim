13b419298a394b905fb0251bf81d2349c763c60d
Integralist <mark.mcdx@gmail.com>
2024-01-17 16:23:42 +0000

    feat(mappings): add zz to n movement

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,40 +1,43 @@
 vim.keymap.set("", "<leader><leader>ps", "<Cmd>Lazy sync<CR>",
-               {desc = "update vim plugins"})
+  { desc = "update vim plugins" })
 
 vim.keymap.set("", "±", "<Cmd>nohlsearch<CR>",
-               {desc = "turn off search highlight"})
+  { desc = "turn off search highlight" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz",
-               {desc = "scroll down and then center the cursorline"})
+  { desc = "scroll down and then center the cursorline" })
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz",
-               {desc = "scroll up and then center the cursorline"})
+  { desc = "scroll up and then center the cursorline" })
 
 vim.keymap.set("n", "zo", "zozz",
-               {desc = "open fold and then center the cursorline"})
+  { desc = "open fold and then center the cursorline" })
 
 vim.keymap.set("n", "zr", "zrzz",
-               {desc = "open fold and then center the cursorline"})
+  { desc = "open fold and then center the cursorline" })
 
 vim.keymap.set("n", "zR", "zRzz",
-               {desc = "open all folds and then center the cursorline"})
+  { desc = "open all folds and then center the cursorline" })
 
 vim.keymap.set("n", "zc", "zczz",
-               {desc = "close fold and then center the cursorline"})
+  { desc = "close fold and then center the cursorline" })
 
 vim.keymap.set("n", "zm", "zmzz",
-               {desc = "close fold and then center the cursorline"})
+  { desc = "close fold and then center the cursorline" })
 
 vim.keymap.set("n", "zM", "zMzz",
-               {desc = "close all folds and then center the cursorline"})
+  { desc = "close all folds and then center the cursorline" })
+
+vim.keymap.set("n", "n", "nzz",
+  { desc = "move to the next search match and then center the cursorline" })
 
 function _G.set_terminal_keymaps()
-    local opts = {buffer = 0}
-    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
