fa4a34f42003b74116f6a5f40c535e5ecf9ab5a6
Integralist <mark.mcdx@gmail.com>
2024-02-07 17:21:02 +0000

    fix(motions): new setup for move.nvim

--- lua/plugins/motions.lua
+++ lua/plugins/motions.lua
@@ -10,21 +10,19 @@ return {
           .set('', 'ge', '<Plug>CamelCaseMotion_ge', { silent = true })
     end
   }, {
-    -- MOVE LINES AROUND
-    "fedepujol/move.nvim",
-    config = function()
-      local opts = { noremap = true, silent = true }
-      -- Normal-mode commands
-      vim.keymap.set('n', '<C-j>', ':MoveLine(1)<CR>', opts)
-      vim.keymap.set('n', '<C-k>', ':MoveLine(-1)<CR>', opts)
-      vim.keymap.set('n', '<C-h>', ':MoveHChar(-1)<CR>', opts)
-      vim.keymap.set('n', '<C-l>', ':MoveHChar(1)<CR>', opts)
+  -- MOVE LINES AROUND
+  "fedepujol/move.nvim",
+  config = function()
+    require('move').setup({})
+    local opts = { noremap = true, silent = true }
 
-      -- Visual-mode commands
-      vim.keymap.set('v', '<S-j>', ':MoveBlock(1)<CR>', opts)
-      vim.keymap.set('v', '<S-k>', ':MoveBlock(-1)<CR>', opts)
-      vim.keymap.set('v', '<S-h>', ':MoveHBlock(-1)<CR>', opts)
-      vim.keymap.set('v', '<S-l>', ':MoveHBlock(1)<CR>', opts)
-    end
-  }
+    -- Normal-mode commands
+    vim.keymap.set('n', '<C-j>', ':MoveLine(1)<CR>', opts)
+    vim.keymap.set('n', '<C-k>', ':MoveLine(-1)<CR>', opts)
+
+    -- Visual-mode commands
+    vim.keymap.set('v', '<S-j>', ':MoveBlock(1)<CR>', opts)
+    vim.keymap.set('v', '<S-k>', ':MoveBlock(-1)<CR>', opts)
+  end
+}
 }
