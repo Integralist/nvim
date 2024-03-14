f4b17651f2ab93dc84707014381cc4b220f1c570
Integralist <mark.mcdx@gmail.com>
2024-03-14 15:59:26 +0000

    format indicators

--- lua/plugins/indicators.lua
+++ lua/plugins/indicators.lua
@@ -2,16 +2,19 @@ return {
   {
     -- WORD USAGE HIGHLIGHTER
     "RRethy/vim-illuminate"
-  }, {
-  -- JUMP TO WORD INDICTORS
-  "jinh0/eyeliner.nvim",
-  lazy = false,
-  opts = { highlight_on_key = true, dim = true }
-}, {
-  -- CURSOR MOVEMENT HIGHLIGHTER
-  "DanilaMihailov/beacon.nvim"
-}, {
-  -- HIGHLIGHT YANKED REGION
-  "machakann/vim-highlightedyank"
-}
+  },
+  {
+    -- JUMP TO WORD INDICTORS
+    "jinh0/eyeliner.nvim",
+    lazy = false,
+    opts = { highlight_on_key = true, dim = true }
+  },
+  {
+    -- CURSOR MOVEMENT HIGHLIGHTER
+    "DanilaMihailov/beacon.nvim"
+  },
+  {
+    -- HIGHLIGHT YANKED REGION
+    "machakann/vim-highlightedyank"
+  },
 }
