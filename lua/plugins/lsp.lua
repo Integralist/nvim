14d72d3f4cb379275d32608129c04e5d3161db33
Integralist <mark.mcdx@gmail.com>
2024-03-21 10:42:39 +0000

    ensure installed autotools_ls for Make

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -306,7 +306,7 @@ return {
     -- ◍ golangci-lint
     mason_lspconfig.setup({
       ensure_installed = {
-        "bashls", "gopls", "jsonls",
+        "autotools_ls", "bashls", "gopls", "jsonls",
         "lua_ls", "marksman", "pylsp",
         "rust_analyzer", "taplo", "terraformls", "tflint", "tsserver",
         "yamlls", "zls"
