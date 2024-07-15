fd84a6f381c85fe8ca82c4415db49ee082950196
Integralist <mark.mcdx@gmail.com>
2024-07-15 11:49:33 +0100

    fix which-keys

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,53 +2,53 @@ return {
   {
     -- NEOVIM DEVELOPMENT SETUP
     "folke/neodev.nvim"
-  }, {
-  -- MAPPING IDENTIFIER
-  "folke/which-key.nvim",
-  config = function()
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>d"] = { name = "todo" },
-      ["<leader>l"] = { name = "lsp" },
-      ["<leader><leader>b"] = { name = "dap/debug" },
-      ["<leader><leader>d"] = { name = "diagnostics/todos" },
-      ["<leader><leader>f"] = { name = "format" },
-      ["<leader><leader>g"] = { name = "git" },
-      ["<leader><leader>l"] = { name = "lsp" },
-      -- ["<leader><leader>n"] = {name = "noice"},
-      ["<leader><leader>o"] = { name = "oil" },
-      ["<leader><leader>p"] = { name = "plugins" },
-      ["<leader><leader>q"] = { name = "quickfix" },
-      ["<leader><leader>r"] = { name = "rust lsp" },
-      ["<leader><leader>t"] = { name = "terminal" }
-    })
-  end
-}, {
-  -- COLORSCHEME
-  "EdenEast/nightfox.nvim",
-  lazy = false,    -- make sure we load this during startup as it is our main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    vim.cmd("colorscheme nightfox")
+  },
+  {
+    -- MAPPING IDENTIFIER
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>d",         group = "todo" },
+        { "<leader>l",         group = "lsp" },
+        { "<leader><leader>b", group = "dap/debug" },
+        { "<leader><leader>d", group = "diagnostics/todos" },
+        { "<leader><leader>f", group = "format" },
+        { "<leader><leader>g", group = "git" },
+        { "<leader><leader>l", group = "lsp" },
+        { "<leader><leader>o", group = "oil" },
+        { "<leader><leader>p", group = "plugins" },
+        { "<leader><leader>q", group = "quickfix" },
+        { "<leader><leader>r", group = "rust" },
+        { "<leader><leader>t", group = "terminal" },
+      }
+    }
+  },
+  {
+    -- COLORSCHEME
+    "EdenEast/nightfox.nvim",
+    lazy = false,    -- make sure we load this during startup as it is our main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd("colorscheme nightfox")
 
-    -- The following was in an autocommand in ./autocommands.lua
-    -- But for some reason wouldn't load until I attempted to switch schemes.
-    -- It looks like some other colourscheme is loaded first.
-    -- That scheme takes precedence over me setting nightfox here.
-    -- Possibly the new default theme that comes with neovim 0.10.0
-    -- So the solution is to set the highlights here and keep them in autocommand.
-    vim.api.nvim_set_hl(0, "illuminatedWord",
-      { fg = "#063970", bg = "#76b5c5" })
-    vim.api.nvim_set_hl(0, "LspReferenceText",
-      { fg = "#063970", bg = "#76b5c5" })
-    vim.api.nvim_set_hl(0, "LspReferenceWrite",
-      { fg = "#063970", bg = "#76b5c5" })
-    vim.api.nvim_set_hl(0, "LspReferenceRead",
-      { fg = "#063970", bg = "#76b5c5" })
-    vim.api.nvim_set_hl(0, 'EyelinerPrimary',
-      { fg = "#FF0000", bold = true, underline = true })
-    vim.api.nvim_set_hl(0, 'EyelinerSecondary',
-      { fg = "#FFFF00", underline = true })
-  end
-},
+      -- The following was in an autocommand in ./autocommands.lua
+      -- But for some reason wouldn't load until I attempted to switch schemes.
+      -- It looks like some other colourscheme is loaded first.
+      -- That scheme takes precedence over me setting nightfox here.
+      -- Possibly the new default theme that comes with neovim 0.10.0
+      -- So the solution is to set the highlights here and keep them in autocommand.
+      vim.api.nvim_set_hl(0, "illuminatedWord",
+        { fg = "#063970", bg = "#76b5c5" })
+      vim.api.nvim_set_hl(0, "LspReferenceText",
+        { fg = "#063970", bg = "#76b5c5" })
+      vim.api.nvim_set_hl(0, "LspReferenceWrite",
+        { fg = "#063970", bg = "#76b5c5" })
+      vim.api.nvim_set_hl(0, "LspReferenceRead",
+        { fg = "#063970", bg = "#76b5c5" })
+      vim.api.nvim_set_hl(0, 'EyelinerPrimary',
+        { fg = "#FF0000", bold = true, underline = true })
+      vim.api.nvim_set_hl(0, 'EyelinerSecondary',
+        { fg = "#FFFF00", underline = true })
+    end
+  },
 }
