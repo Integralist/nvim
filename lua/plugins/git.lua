0db33c1d13e5c6f1a9c3bfba5bde6b80ce746f9f
Integralist <mmcdonnell@fastly.com>
2024-08-21 10:25:25 +0100

    dont suppress octo error

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -114,9 +114,10 @@ return {
       'nvim-tree/nvim-web-devicons',
     },
     opts = {
-      suppress_missing_scope = {
-        projects_v2 = true,   -- Suppresses the error: Cannot request projects v2, missing scope 'read:project'
-      }
+      default_to_projects_v2 = true,
+      -- suppress_missing_scope = {
+      --   projects_v2 = true,   -- Suppresses the error: Cannot request projects v2, missing scope 'read:project'
+      -- }
     }
   },
   {
