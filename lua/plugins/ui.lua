bb4d05bd69007475af2ca4ba0139cdb0ea9a1196
Integralist <mmcdonnell@fastly.com>
2024-11-25 07:00:27 +0000

    theme: bubbles

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,33 @@
+-- The following local variables are for the 'bubble' theme for lualine.
+
+local colors = {
+	blue   = '#80a0ff',
+	cyan   = '#79dac8',
+	black  = '#080808',
+	white  = '#c6c6c6',
+	red    = '#ff5189',
+	violet = '#d183e8',
+	grey   = '#303030',
+}
+
+local bubbles_theme = {
+	normal = {
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.white },
+	},
+
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
+
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.white },
+	},
+}
+
 return {
 	{
 		-- COLORSCHEME
@@ -43,14 +73,16 @@ return {
 		event = "UIEnter",
 		config = function()
 			require("lualine").setup({
+				options = {
+					theme = bubbles_theme,
+					component_separators = '',
+					section_separators = { left = '', right = '' },
+				},
 				sections = {
+					lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+					lualine_b = { { 'filename', file_status = true, path = 1, shorting_target = 40 }, 'diagnostics', 'branch', 'diff' },
 					lualine_c = {
-						{
-							"filename",
-							file_status = true, -- displays file status (readonly status, modified status)
-							path = 1,     -- relative path
-							shorting_target = 40 -- Shortens path to leave 40 space in the window
-						}
+						'%=', --[[ add your center compoentnts here in place of this comment ]]
 					},
 					lualine_x = {
 						{
@@ -77,12 +109,64 @@ return {
 									end,
 								})
 							end,
-							icon = { "", align = "right" },
+							icon = { "֎", align = "right" },
 						},
-						"diagnostics",
 					},
-					lualine_y = { "filetype", "encoding", "fileformat" },
-					lualine_z = { "location" },
+					lualine_y = { 'filetype', 'encoding', 'fileformat', 'progress' }, -- progress is percentage of file scrolled
+					lualine_z = {
+						{ 'location', separator = { right = '' }, left_padding = 2 },
+					},
+					-- DISABLED:
+					-- Traditional/classic lualine theme
+					--
+					-- 	lualine_c = {
+					-- 		{
+					-- 			"filename",
+					-- 			file_status = true, -- displays file status (readonly status, modified status)
+					-- 			path = 1,     -- relative path
+					-- 			shorting_target = 40 -- Shortens path to leave 40 space in the window
+					-- 		}
+					-- 	},
+					-- 	lualine_x = {
+					-- 		{
+					-- 			function()
+					-- 				return require("lsp-progress").progress({
+					-- 					max_size = 80,
+					-- 					format = function(messages)
+					-- 						local bufnr = vim.api.nvim_get_current_buf()
+					-- 						local active_clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+					-- 						if #messages > 0 then
+					-- 							return table.concat(messages, " ")
+					-- 						end
+					-- 						local client_names = {}
+					-- 						for _, client in ipairs(active_clients) do
+					-- 							if client and client.name ~= "" then
+					-- 								table.insert(
+					-- 									client_names,
+					-- 									1,
+					-- 									client.name
+					-- 								)
+					-- 							end
+					-- 						end
+					-- 						return table.concat(client_names, "  ")
+					-- 					end,
+					-- 				})
+					-- 			end,
+					-- 			icon = { "", align = "right" },
+					-- 		},
+					-- 		"diagnostics",
+					-- 	},
+					-- 	lualine_y = { "filetype", "encoding", "fileformat" },
+					-- 	lualine_z = { "location" },
+					-- }
+				},
+				inactive_sections = {
+					lualine_a = { 'filename' },
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = { 'location' },
 				}
 			})
 			vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
