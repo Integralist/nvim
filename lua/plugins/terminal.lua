78a3196f7b5f822db4b2eb43fff2246d6590763f
Integralist <mmcdonnell@fastly.com>
2024-11-28 11:17:55 +0000

    set width on floating terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,31 +1,36 @@
 return {
-  {
-    -- TERMINAL
-    "akinsho/toggleterm.nvim",
-    version = "v2.*",
-    config = function()
-      require("toggleterm").setup()
+	{
+		-- TERMINAL
+		"akinsho/toggleterm.nvim",
+		version = "v2.*",
+		config = function()
+			require("toggleterm").setup({
+				float_opts = {
+					border = "curved",
+					width = 150
+				},
+			})
 
-      local Terminal = require('toggleterm.terminal').Terminal
-      local htop = Terminal:new({
-        cmd = "htop",
-        hidden = true,
-        direction = "float"
-      })
+			local Terminal = require('toggleterm.terminal').Terminal
+			local htop = Terminal:new({
+				cmd = "htop",
+				hidden = true,
+				direction = "float"
+			})
 
-      -- NOTE: This is a global function so it can be called from the below mapping.
-      function Htop_toggle() htop:toggle() end
+			-- NOTE: This is a global function so it can be called from the below mapping.
+			function Htop_toggle() htop:toggle() end
 
-      vim.keymap.set("n", "<leader><leader>th",
-        "<cmd>lua Htop_toggle()<CR>", {
-          noremap = true,
-          silent = true,
-          desc = "toggle htop"
-        })
+			vim.keymap.set("n", "<leader><leader>th",
+				"<cmd>lua Htop_toggle()<CR>", {
+					noremap = true,
+					silent = true,
+					desc = "toggle htop"
+				})
 
-      vim.keymap.set("n", "<leader><leader>tf",
-        "<Cmd>ToggleTerm direction=float<CR>",
-        { desc = "toggle floating terminal" })
-    end
-  }
+			vim.keymap.set("n", "<leader><leader>tf",
+				"<Cmd>ToggleTerm direction=float<CR>",
+				{ desc = "toggle floating terminal" })
+		end
+	}
 }
