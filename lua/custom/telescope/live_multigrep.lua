ddefc6b8067c485042b6f47b18cc1bc664b388e9
Integralist <mmcdonnell@fastly.com>
2024-12-18 10:34:16 +0000

    use long flags to be more descriptive

--- lua/custom/telescope/live_multigrep.lua
+++ lua/custom/telescope/live_multigrep.lua
@@ -18,12 +18,12 @@ local live_multigrep = function(opts)
 			local pieces = vim.split(prompt, "  ")
 			local args = { "rg" }
 			if pieces[1] then
-				table.insert(args, "-e")
+				table.insert(args, "--regexp")
 				table.insert(args, pieces[1])
 			end
 
 			if pieces[2] then
-				table.insert(args, "-g")
+				table.insert(args, "--glob")
 				table.insert(args, pieces[2])
 			end
 
