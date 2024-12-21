52730b14654f90f47debe359bed9e4ca2f43e505
Integralist <mmcdonnell@fastly.com>
2024-12-21 12:05:12 +0000

    terminal fixes

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -32,13 +32,29 @@ return {
 				"<Cmd>ToggleTerm direction=float<CR>",
 				{ desc = "toggle floating terminal" })
 
-			vim.keymap.set("n", "<leader><leader>ts",
-				"<Cmd>ToggleTerm direction=horizontal size=30<CR>",
-				{ desc = "toggle split terminal" })
+			-- vim.keymap.set("n", "<leader><leader>ts",
+			-- 	"<Cmd>ToggleTerm direction=horizontal size=30<CR>",
+			-- 	{ desc = "open split terminal" })
+			--
+			-- vim.keymap.set("n", "<leader><leader>tv",
+			-- 	"<Cmd>ToggleTerm direction=vertical size=120<CR>",
+			-- 	{ desc = "open vertical terminal" })
 
-			vim.keymap.set("n", "<leader><leader>tv",
-				"<Cmd>ToggleTerm direction=vertical size=120<CR>",
-				{ desc = "toggle vertical terminal" })
+			-- :Ts some_name
+			vim.api.nvim_create_user_command('Ts', function(opts)
+				vim.cmd('sp term://zsh')
+				vim.cmd('set nonumber')
+				vim.cmd('set nospell')
+				vim.cmd('file ' .. opts.args)
+			end, { nargs = 1 })
+
+			-- :Tv some_name
+			vim.api.nvim_create_user_command('Tv', function(opts)
+				vim.cmd('vs term://zsh')
+				vim.cmd('set nonumber')
+				vim.cmd('set nospell')
+				vim.cmd('file ' .. opts.args)
+			end, { nargs = 1 })
 		end
 	}
 }
