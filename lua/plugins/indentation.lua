247e757324d16e00dc2af3205267840df6a5950b
Integralist <mmcdonnell@fastly.com>
2025-01-06 16:05:11 +0000

    format: indentation

--- lua/plugins/indentation.lua
+++ lua/plugins/indentation.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    -- INDENTATION AUTOPAIRING
-    "windwp/nvim-autopairs",
-    config = true,
-  },
-  {
-    -- WHITESPACE MANAGEMENT
-    "kaplanz/retrail.nvim",
-    opts = {
-      filetype = {
-        exclude = {
-          "markdown", "neo-tree",
-          -- following are defaults that need to be added or they'll be overridden
-          "", "aerial", "alpha", "checkhealth", "cmp_menu", "diff", "help",
-          "lazy", "lspinfo", "man", "mason", "TelescopePrompt", "toggleterm",
-          "Trouble", "WhichKey"
-        }
-      }
-    }
-  }
+	{
+		-- INDENTATION AUTOPAIRING
+		"windwp/nvim-autopairs",
+		config = true,
+	},
+	{
+		-- WHITESPACE MANAGEMENT
+		"kaplanz/retrail.nvim",
+		opts = {
+			filetype = {
+				exclude = {
+					"markdown", "neo-tree",
+					-- following are defaults that need to be added or they'll be overridden
+					"", "aerial", "alpha", "checkhealth", "cmp_menu", "diff", "help",
+					"lazy", "lspinfo", "man", "mason", "TelescopePrompt", "toggleterm",
+					"Trouble", "WhichKey"
+				}
+			}
+		}
+	}
 }
