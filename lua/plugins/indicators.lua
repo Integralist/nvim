d8a868497c7f815f6f0a9ae1420021ba36a41105
Integralist <mmcdonnell@fastly.com>
2025-01-06 16:05:26 +0000

    format: indicators

--- lua/plugins/indicators.lua
+++ lua/plugins/indicators.lua
@@ -1,30 +1,30 @@
 return {
-  {
-    -- WORD USAGE HIGHLIGHTER
-    "RRethy/vim-illuminate"
-  },
-  {
-    -- JUMP TO WORD INDICTORS
-    "jinh0/eyeliner.nvim",
-    lazy = false,
-    opts = { highlight_on_key = true, dim = true }
-  },
-  {
-    -- CURSOR MOVEMENT HIGHLIGHTER
-    "DanilaMihailov/beacon.nvim",
-    opts = {
-      min_jump = 5,
-      speed = 10,
-    },
-  },
-  {
-    -- HIGHLIGHT YANKED REGION
-    "machakann/vim-highlightedyank"
-  },
-  -- {
-  --   "tris203/precognition.nvim",
-  --   opts = {
-  --     startVisible = true
-  --   }
-  -- }
+	{
+		-- WORD USAGE HIGHLIGHTER
+		"RRethy/vim-illuminate"
+	},
+	{
+		-- JUMP TO WORD INDICTORS
+		"jinh0/eyeliner.nvim",
+		lazy = false,
+		opts = { highlight_on_key = true, dim = true }
+	},
+	{
+		-- CURSOR MOVEMENT HIGHLIGHTER
+		"DanilaMihailov/beacon.nvim",
+		opts = {
+			min_jump = 5,
+			speed = 10,
+		},
+	},
+	{
+		-- HIGHLIGHT YANKED REGION
+		"machakann/vim-highlightedyank"
+	},
+	-- {
+	--   "tris203/precognition.nvim",
+	--   opts = {
+	--     startVisible = true
+	--   }
+	-- }
 }
