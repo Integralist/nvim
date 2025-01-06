dd684230c9befab9cba19cbab21a9fe30ade3ab9
Integralist <mmcdonnell@fastly.com>
2025-01-06 16:00:24 +0000

    format: init

--- init.lua
+++ init.lua
@@ -2,17 +2,17 @@
 -- :h standard-path (~/.local/share/nvim)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,13 +23,13 @@ vim.opt.rtp:prepend(lazypath)
 -- https://lazy.folke.io/spec
 --
 require("lazy").setup({
-  checker = {
-    enabled = true,
-    notify = true
-  },
-  spec = {
-    { import = "plugins" }
-  },
+	checker = {
+		enabled = true,
+		notify = true
+	},
+	spec = {
+		{ import = "plugins" }
+	},
 })
 
 require("autocommands")
