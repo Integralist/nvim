e0d3b74e4f02cf0ae565bcfdd417ccbf7e4d7307
Integralist <mmcdonnell@fastly.com>
2025-01-07 14:51:52 +0000

    writing: add markdown preview

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -1,5 +1,26 @@
 return {
 	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons"
+		},
+		opts = {
+			initial_state = false
+		},
+		keys = {
+			{
+				"<leader><leader>v",
+				"<Cmd>Markview toggle<CR>",
+				desc = "toggle markdown preview",
+				mode = "n",
+				noremap = true,
+				silent = true
+			}
+		}
+	},
+	{
 		-- WRITING
 		"marcelofern/vale.nvim",
 		opts = {
