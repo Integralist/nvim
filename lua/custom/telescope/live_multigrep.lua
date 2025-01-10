aea8b01fd1eea27fb641987128d62122d271d9a9
Integralist <mmcdonnell@fastly.com>
2025-01-10 15:05:12 +0000

    fix: rg shouldn't respect ignore files

--- lua/custom/telescope/live_multigrep.lua
+++ lua/custom/telescope/live_multigrep.lua
@@ -29,7 +29,7 @@ local live_multigrep = function(opts)
 
 			---@diagnostic disable-next-line: deprecated
 			return vim.tbl_flatten({ args,
-				{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" } }
+				{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--no-ignore", "--hidden" } }
 			)
 		end,
 		entry_maker = make_entry.gen_from_vimgrep(opts),
