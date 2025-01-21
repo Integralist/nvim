dd595e35a981ed7066e51a2c7577890b34d22626
Integralist <mmcdonnell@fastly.com>
2025-01-21 18:00:44 +0000

    feat: dim inactive window

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -113,3 +113,25 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	pattern = "*.go",
 	command = "silent !goimports -w %"
 })
+
+-- Dim inactive windows
+
+vim.cmd("highlight default DimInactiveWindows guifg=#666666")
+
+-- When leaving a window, set all highlight groups to a "dimmed" hl_group
+vim.api.nvim_create_autocmd({ "WinLeave" }, {
+	callback = function()
+		local highlights = {}
+		for hl, _ in pairs(vim.api.nvim_get_hl(0, {})) do
+			table.insert(highlights, hl .. ":DimInactiveWindows")
+		end
+		vim.wo.winhighlight = table.concat(highlights, ",")
+	end,
+})
+
+-- When entering a window, restore all highlight groups to original
+vim.api.nvim_create_autocmd({ "WinEnter" }, {
+	callback = function()
+		vim.wo.winhighlight = ""
+	end,
+})
