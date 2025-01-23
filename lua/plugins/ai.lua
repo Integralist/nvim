9f49756e0ab0669fe9bbedf98bcc2786b4c2fc12
Integralist <mmcdonnell@fastly.com>
2025-01-23 11:16:28 +0000

    ai

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,20 @@
+return {
+	-- {
+	-- 	'github/copilot.vim', -- :Copilot setup
+	-- },
+	-- {
+	-- 	"jackMort/ChatGPT.nvim",
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		require("chatgpt").setup({
+	-- 			api_key_cmd = "op read op://private/OpenAI/credential --no-newline"
+	-- 		})
+	-- 	end,
+	-- 	dependencies = {
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"folke/trouble.nvim",
+	-- 		"nvim-telescope/telescope.nvim"
+	-- 	}
+	-- }
+}
