4967935f9ef2a9adc09b2a5b2c939cc911ebf631
Integralist <mmcdonnell@fastly.com>
2025-05-08 17:28:00 +0100

    disable hints by default

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -152,7 +152,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		local bufnr = args.buf ---@type number
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client.supports_method('textDocument/inlayHint') then
-			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+			vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
 			vim.keymap.set('n', '<leader><leader>lh', function()
 				vim.lsp.inlay_hint.enable(
 					not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
