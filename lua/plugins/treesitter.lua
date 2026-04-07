c6f36016cf029307c6854a8136c11a38f1a1393d
Integralist <mark.mcdx@gmail.com>
2026-04-07 10:29:21 +0100

    migrate treesitter plugins to new main branch for nvim 0.12
    The nvim-treesitter plugin was rewritten on a new `main` branch;
    the old `master` branch is frozen and incompatible with Neovim 0.12.
    This updates the treesitter config to use the new API: FileType
    autocmd for highlighting/indentation, explicit parser installation,
    and manual keymaps for textobjects.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,106 +2,96 @@ return {
 	{
 		-- TREESITTER
 		-- Syntax tree parsing for more intelligent syntax highlighting and code navigation
-		-- IMPORTANT: If there are issues try `:TSInstall all` or `:TSUpdate`.
+		-- IMPORTANT: Requires `tree-sitter` CLI: brew install tree-sitter-cli
+		-- After switching branches run: :TSUninstall all, restart, :TSUpdate
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"cmake",
-					"css",
-					"dockerfile",
-					"go",
-					"gomod",
-					"gowork",
-					"hcl",
-					"html",
-					"http",
-					"javascript",
-					"json",
-					"lua",
-					"make",
-					"markdown",
-					"python",
-					"regex",
-					"ruby",
-					"rust",
-					"terraform",
-					"toml",
-					"vim",
-					"yaml",
-					"zig",
-				},
-				highlight = { enable = true },
-				rainbow = {
-					enable = true,
-					extended_mode = true,
-					max_file_lines = nil
-				}
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					pcall(vim.treesitter.start)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
 			})
-		end
+		end,
+		config = function()
+			local ensure_installed = {
+				"bash",
+				"c",
+				"cmake",
+				"css",
+				"dockerfile",
+				"go",
+				"gomod",
+				"gowork",
+				"hcl",
+				"html",
+				"http",
+				"javascript",
+				"json",
+				"lua",
+				"make",
+				"markdown",
+				"python",
+				"regex",
+				"ruby",
+				"rust",
+				"terraform",
+				"toml",
+				"vim",
+				"yaml",
+				"zig",
+			}
+
+			local already_installed = require("nvim-treesitter").get_installed()
+			local to_install = vim.iter(ensure_installed)
+				:filter(function(parser)
+					return not vim.tbl_contains(already_installed, parser)
+				end)
+				:totable()
+			if #to_install > 0 then
+				require("nvim-treesitter").install(to_install)
+			end
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "gnn",    -- start treesitter selection process
-						scope_incremental = "gnm", -- increment selection to surrounding scope
-						node_incremental = ";",    -- increment selection to next 'node'
-						node_decremental = ","     -- decrement selection to prev 'node'
-					}
-				},
-				indent = { enable = true },
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						include_surrounding_whitespace = false,
-						keymaps = {
-							["af"] = {
-								query = "@function.outer",
-								desc = "select around a function"
-							},
-							["if"] = {
-								query = "@function.inner",
-								desc = "select inner part of a function"
-							},
-							["ac"] = {
-								query = "@class.outer",
-								desc = "select around a class"
-							},
-							["ic"] = {
-								query = "@class.inner",
-								desc = "select inner part of a class"
-							}
-						},
-						selection_modes = {
-							['@parameter.outer'] = 'v',
-							['@function.outer'] = 'V',
-							['@class.outer'] = '<c-v>'
-						}
+			local select = require("nvim-treesitter-textobjects.select")
+			local move = require("nvim-treesitter-textobjects.move")
+
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+					include_surrounding_whitespace = false,
+					selection_modes = {
+						["@parameter.outer"] = "v",
+						["@function.outer"] = "V",
+						["@class.outer"] = "<c-v>",
 					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]]"] = "@function.outer",
-							["]\\"] = "@class.outer"
-						},
-						goto_previous_start = {
-							["[["] = "@function.outer",
-							["[\\"] = "@class.outer"
-						}
-					}
-				}
+				},
+				move = {
+					set_jumps = true,
+				},
 			})
-		end
+
+			local function map(mode, lhs, rhs, desc)
+				vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+			end
+
+			map({ "x", "o" }, "af", function() select.select_textobject("@function.outer") end, "select around a function")
+			map({ "x", "o" }, "if", function() select.select_textobject("@function.inner") end, "select inner part of a function")
+			map({ "x", "o" }, "ac", function() select.select_textobject("@class.outer") end, "select around a class")
+			map({ "x", "o" }, "ic", function() select.select_textobject("@class.inner") end, "select inner part of a class")
+
+			map({ "n", "x", "o" }, "]]", function() move.goto_next_start("@function.outer") end, "next function start")
+			map({ "n", "x", "o" }, "]\\", function() move.goto_next_start("@class.outer") end, "next class start")
+			map({ "n", "x", "o" }, "[[", function() move.goto_previous_start("@function.outer") end, "prev function start")
+			map({ "n", "x", "o" }, "[\\", function() move.goto_previous_start("@class.outer") end, "prev class start")
+		end,
 	},
 	{
 		-- HIGHLIGHT ARGUMENTS' DEFINITIONS AND USAGES, USING TREESITTER
@@ -111,7 +101,6 @@ return {
 	},
 	{
 		-- SHOWS THE CONTEXT OF THE CURRENTLY VISIBLE BUFFER CONTENTS
-		-- 🚨 This has stopped working. Not sure why.
 		"nvim-treesitter/nvim-treesitter-context",
 		dependencies = "nvim-treesitter/nvim-treesitter",
 		opts = { separator = "-" }
